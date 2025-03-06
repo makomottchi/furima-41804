4b5f703eae7914bd190973684ae46f38f870eec4
makomottchi <hinatatc@yahoo.co.jp>
2025-03-06 21:38:15 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
+         validates :nickname, presence: true
+         validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角ひらがなカタカナ漢字で入力してください' }, if: -> { family_name.present }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角ひらがなカタカナ漢字で入力してください' }, if: -> { first_name.present }
+         validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }, if: -> { family_name_kana.present? }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }, if: -> { first_name_kana.present? }
+         validates :birth_day, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合で入力してください' }, if: -> { password.present? }
 end
