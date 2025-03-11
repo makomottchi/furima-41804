3febaec1dff64c4dd5e3262b9660965ff0d49a06
makomottchi <hinatatc@yahoo.co.jp>
2025-03-12 08:23:02 +0900

    createアクションの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,18 @@ def new
   @product = Product.new
 end
 
+def create
+  @product = Product.new(product_params)
+  if @product.save
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
 private
 
-def message_params
+def product_params
   params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
 end
 
