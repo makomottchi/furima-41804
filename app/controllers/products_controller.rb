cb14a08cf63ae59e5813e53ce1137383ebeab015
makomottchi <hinatatc@yahoo.co.jp>
2025-03-12 08:19:05 +0900

    index.html.erbのnew_product_path設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,17 @@
 class ProductsController < ApplicationController
 
 def index
+  @product = Product.all
+end
+
+def new
+  @product = Product.new
+end
+
+private
+
+def message_params
+  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'products/index'
   root to: 'products#index'
+
+  resources :products, only: [:index, :create, :new]
+
 end
