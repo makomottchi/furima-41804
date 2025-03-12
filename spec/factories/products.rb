6de9ae14560c61d7ecd9cf1d8c942dce4b955b87
makomottchi <hinatatc@yahoo.co.jp>
2025-03-12 09:39:44 +0900

    Productモデルの単体テストコードの記述

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :product do
-    
+    name { "テスト商品" }
+    description { "これはテスト商品の説明です。" }
+    category_id { 2 } # メンズなどの適切なカテゴリIDを設定
+    status_id { 2 } # 新品・未使用などの適切な状態IDを設定
+    shipping_cost_id { 2 } # 着払いなどの適切な配送費負担IDを設定
+    prefecture_id { 2 } # 北海道などの適切な都道府県IDを設定
+    shipping_day_id { 2 } # 1~2日で発送などの適切な発送日数IDを設定
+    price { 500 }
+    association :user # 商品を持っているユーザーを設定
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product = FactoryBot.build(:product)
+    @product.image.attach(io: File.open('spec/fixtures/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+  end
+
+  describe '商品新規登録' do
+    context '新規登録できるとき' do
+      it '全ての項目が存在すれば登録できる' do
+        expect(@product).to be_valid
+      end
+    end
+
+    context '新規登録できないとき' do
+      it '商品画像が無いと登録できない' do
+        @product.image = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '商品名が無いと登録できない' do
+        @product.name = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it '商品の説明が無いと登録できない' do
+        @product.description = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'カテゴリー情報が無いと登録できない' do
+        @product.category_id = 1 # "---"のidを1としている場合
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '商品の状態情報が無いと登録できない' do
+        @product.status_id = 1 # "---"のidを1としている場合
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Status can't be blank")
+      end
+
+      it '配送料の負担情報が無いと登録できない' do
+        @product.shipping_cost_id = 1 # "---"のidを1としている場合
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Shipping_cost can't be blank")
+      end
+
+      it '発送元の地域情報が無いと登録できない' do
+        @product.prefecture_id = 1 # "---"のidを1としている場合
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '発送までの日数情報が無いと登録できない' do
+        @product.shipping_day_id = 1 # "---"のidを1としている場合
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Shipping_day can't be blank")
+      end
+
+      it '価格情報が無いと登録できない' do
+        @product.price = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price can't be blank")
+      end
+    end
+  end
 end
