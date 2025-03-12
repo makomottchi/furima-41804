02793ef183449b4b453c33e0523664039eeac20c
makomottchi <hinatatc@yahoo.co.jp>
2025-03-12 19:51:20 +0900

    エラーハンドリングの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,11 +7,7 @@ end
 
 def new
   @product = Product.new
-  @categories = Category.all
-  @statuses = Status.all
-  @shipping_costs = ShippingCost.all
-  @shipping_days = ShippingDay.all
-  @prefectures = Prefecture.all  
+  set_collections
 end
 
 def create
@@ -19,20 +15,25 @@ def create
   if @product.save
     redirect_to root_path
   else
+    set_collections
     render :new, status: :unprocessable_entity
   end
 end
 
 def edit
   @product = Product.find(params[:id])
-  @categories = Category.all
-  @statuses = Status.all
-  @shipping_costs = Shipping_cost.all
-  @shipping_days = Shipping_day.all
-  @prefectures = Prefecture.all
+  set_collections
 end
 
 private
+
+def set_collections
+  @categories = Category.all
+  @statuses = Status.all
+  @shipping_costs = ShippingCost.all
+  @shipping_days = ShippingDay.all
+  @prefectures = Prefecture.all
+end
 
 def product_params
   params.require(:product).permit(:name, :price, :description,:image,:category_id,:status_id,:shipping_cost_id,:shipping_day_id,:prefecture_id).merge(user_id: current_user.id)
